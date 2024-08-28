bbb0a3237eca3a1448dd834e40eefaad9f6ddafe
WaydCloud <108177614+WaydCloud@users.noreply.github.com>
2024-08-29 00:51:09 +0900

    Add files via upload

--- firebase/load.py
+++ firebase/load.py
@@ -2,8 +2,8 @@ import firebase_admin
 from firebase_admin import firestore, credentials
 import asyncio
 import logging
-import os
-from google.oauth2 import service_account
+import datetime
+from datetime import timedelta
 
 logger = logging.getLogger(__name__)
 
@@ -43,7 +43,22 @@ async def load_all_artists():
     except Exception as e:
         print(f"Failed to load artists from Firebase: {e}")
         return None
-    
+
+async def load_artist_albums(artist_id):
+    try:
+        db = firestore.client()
+        
+        songs_ref = db.collection('albums')
+        query = songs_ref.where('artist_id', '==', artist_id)
+        docs = await asyncio.to_thread(query.stream)
+
+        albums = [doc.to_dict() for doc in docs]
+
+        return albums
+    except Exception as e:
+        print(f"Failed to load albums from Firestore: {str(e)}")
+        return []
+
 async def load_artist_songs(artist_id):
     try:
         db = firestore.client()
@@ -58,3 +73,84 @@ async def load_artist_songs(artist_id):
     except Exception as e:
         print(f"Failed to load songs from Firestore: {str(e)}")
         return []
+    
+async def load_artist_videos(artist_id):
+    try:
+        db = firestore.client()
+        
+        videos_ref = db.collection('videos')
+        query = videos_ref.where('artist_id', '==', artist_id)
+        docs = await asyncio.to_thread(query.stream)
+
+        videos = [doc.to_dict() for doc in docs]
+
+        return videos
+    except Exception as e:
+        print(f"Failed to load videos from Firestore: {str(e)}")
+        return []
+    
+async def load_artist_photos(artist_id):
+    try:
+        db = firestore.client()
+        
+        photos_ref = db.collection('photos')
+        query = photos_ref.where('artist_id', '==', artist_id)
+        docs = await asyncio.to_thread(query.stream)
+
+        photos = [doc.to_dict() for doc in docs]
+
+        return photos
+    except Exception as e:
+        print(f"Failed to load photos from Firestore: {str(e)}")
+        return []
+    
+async def load_album_comments(album_id):
+    try:
+        db = firestore.client()
+        
+        comments_ref = db.collection('comments')
+        query = comments_ref.where('album_id', '==', album_id)
+        docs = await asyncio.to_thread(query.stream)
+
+        comments = [doc.to_dict() for doc in docs]
+
+        return comments
+    except Exception as e:
+        print(f"Failed to load songs from Firestore: {str(e)}")
+        return []
+    
+async def load_artist_comments(artist_id):
+    try:
+        db = firestore.client()
+        
+        comments_ref = db.collection('comments')
+        query = comments_ref.where('artist_id', '==', artist_id)
+        docs = await asyncio.to_thread(query.stream)
+
+        comments = [doc.to_dict() for doc in docs]
+
+        return comments
+    except Exception as e:
+        print(f"Failed to load songs from Firestore: {str(e)}")
+        return []
+    
+async def load_latest_comments(artist_id):
+    try:
+        db = firestore.client()
+        
+        comments_ref = db.collection('comments')
+        query = comments_ref.where('artist_id', '==', artist_id).order_by('updatedAt', direction=firestore.Query.DESCENDING)
+        docs = await asyncio.to_thread(query.stream)
+        comments = [doc.to_dict() for doc in docs]
+
+        if not comments:
+            return None
+        
+        for comment in comments:
+            if 'updatedAt' not in comment:
+                comment['updatedAt'] = None
+
+        return comments
+    except Exception as e:
+        print(f"Failed to load songs from Firestore: {str(e)}")
+        return None
--- firebase/save.py
+++ firebase/save.py
@@ -1,10 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import asyncio
-import os
-from google.oauth2 import service_account
 import logging
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 if not firebase_admin._apps:
@@ -17,8 +16,12 @@ async def save_artist(artist: dict) -> bool:
         db = firestore.client()
         collection_ref = db.collection('artists')
         artist_id = artist['id']
+        artist_data = {
+            **artist,
+            'updatedAt': firestore.SERVER_TIMESTAMP
+        }
 
-        await asyncio.to_thread(collection_ref.document(artist_id).set, artist)
+        await asyncio.to_thread(collection_ref.document(artist_id).set, artist_data)
         return True
     except Exception as e:
         return False
@@ -30,7 +33,11 @@ async def save_albums(albums: list) -> bool:
 
         for album in albums:
             album_ref = db.collection('albums').document(album['id'])
-            batch.set(album_ref, album)
+            album_data = {
+                **album,
+                'updatedAt': firestore.SERVER_TIMESTAMP
+            }
+            batch.set(album_ref, album_data)
 
         await asyncio.to_thread(batch.commit)
         return True
@@ -45,6 +52,63 @@ async def save_songs(songs: list) -> bool:
         for song in songs:
             song_ref = db.collection('songs').document(str(song['id']))
             batch.set(song_ref, song)
+
+        await asyncio.to_thread(batch.commit)
+        return True
+    except Exception as e:
+        return False
+    
+async def save_videos(videos: list) -> bool:
+    try:
+        for video in videos:
+            logger.info(f"Videos from save_videos: {video}")
+            
+        db = firestore.client()
+        batch = db.batch()
+
+        for video in videos:
+            video_ref = db.collection('videos').document(video['id'])
+            video_data = {
+                **video,
+                'updatedAt': firestore.SERVER_TIMESTAMP
+            }
+            batch.set(video_ref, video_data)
+
+        await asyncio.to_thread(batch.commit)
+        return True
+    except Exception as e:
+        return False
+    
+async def save_photos(photos: list) -> bool:
+    try:
+        db = firestore.client()
+        batch = db.batch()
+
+        for photo in photos:
+            photo_ref = db.collection('photos').document(photo['id'])
+            photo_data = {
+                **photo,
+                'updatedAt': firestore.SERVER_TIMESTAMP
+            }
+            batch.set(photo_ref, photo_data)
+
+        await asyncio.to_thread(batch.commit)
+        return True
+    except Exception as e:
+        return False
+    
+async def save_comments(comments: list) -> bool:
+    try:
+        db = firestore.client()
+        batch = db.batch()
+
+        for comment in comments:
+            comment_ref = db.collection('comments').document(str(comment['id']))
+            comment_data = {
+                **comment,
+                'updatedAt': firestore.SERVER_TIMESTAMP
+            }
+            batch.set(comment_ref, comment_data)
 
         await asyncio.to_thread(batch.commit)
         return True
